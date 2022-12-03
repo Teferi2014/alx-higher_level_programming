9228deb87491c26bbad66ae5c974254500cdc353
Teferi2014 <teferitirfe@gmail.com>
2022-12-03 11:01:57 -0800

    0x00-python-data_stractures

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
-#!usr/bin/python3
-def element_at(my_list, indx):
-    tam = len(my_list)
-    if indx < 0 or indx >= tam or tam == 0:
-        return None
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    listlength = len(my_list) - 1
+    if (idx < 0 or idx > listlength):
+        return (None)
     else:
-        return my_list[indx]
+        return (my_list[idx])
