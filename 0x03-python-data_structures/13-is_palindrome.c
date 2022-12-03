06f2a9ea80b38a562c6f6ac0efe44e879d9a9a0a
Teferi2014 <teferitirfe@gmail.com>
2022-12-03 11:34:10 -0800

    palendrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,12 +1,41 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include <string.h>
 #include "lists.h"
 /**
- *is_palindrome - ispalindrome
- *@head: list
- *Return: number elements.
+ * palindrome - check if is palindrome with recursion
+ * @l: l
+ * @r: r
+ *
+ * Return: 1 palindrome, 0 not palindrome
+ */
+int palindrome(listint_t **l, listint_t *r)
+{
+	int response;
+
+	if (r != NULL)
+	{
+		response = palindrome(l, r->next);
+		if (response != 0)
+		{
+			response = (r->n == (*l)->n);
+			*l = (*l)->next;
+			return (response);
+		}
+		return (0);
+
+	}
+	return (1);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: head of linked list
+ *
+ * Return: 1 palindrome, 0 not palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	return (0);
+	if (head == NULL)
+	{
+		return (0);
+	}
+	return (palindrome(head, *head));
+}
