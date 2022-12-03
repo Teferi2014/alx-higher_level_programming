a48aea7b85bdc6656f8861f3ebf51495930d580d
Teferi2014 <teferitirfe@gmail.com>
2022-12-03 11:13:32 -0800

    0x03-python_data_stractures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        list = [len(sentence), None]
-        new_tuplex = tuple(list)
+        my_tuple = (0, None)
     else:
-        list = [len(sentence), sentence[0]]
-        new_tuplex = tuple(list)
-    return 
+        my_tuple = (len(sentence), sentence[:1])
+    return(my_tuple)
