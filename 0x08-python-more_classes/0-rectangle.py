65c03d6d74ba5723a4f28ea0d22a01d62d8a7f83
Teferi2014 <teferitirfe@gmail.com>
2023-01-03 00:11:02 -0800

    0x08. Python - More Classes and Objects

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+
+    0-rectangle module
+
+    Empty class Rectangle that defines a rectangle
+
+"""
+
+
+
+
+
+class Rectangle:
+
+    """
+
+        Empty class Rectangle that defines a rectangle
+
+    """
+
+    pass
