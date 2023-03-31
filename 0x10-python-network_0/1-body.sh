f36cbf43d5f175042eecb5056a2f4c2f6b40240b
Teferi2014 <teferitirfe@gmail.com>
2023-03-30 23:21:35 -0700

    python network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# shellcheck disable=SC2046
-if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
+# sends GET request to the URL and displays the body of the response
+curl -sfL "$1" -X GET
